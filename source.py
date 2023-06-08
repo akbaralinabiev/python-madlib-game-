5b31fd5022e59b20af3e239d3b06dc297d7bc420
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-06-08 14:53:36 +0200

    Merge changes from remote repository

---
+++ source.py
@@ -0,0 +1,9 @@
+adj = input("Enter adjective: ")
+verb1 = input("Enter verb: ")
+verb2 = input("Enter one more verb: ")
+famous_person = input("Enter famous person's name: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because\
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
